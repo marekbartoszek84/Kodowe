aaffb76219499deb732ffd23c11afd1c4f92b307
admin <admin@puplebork.local>
2016-12-20 12:54:15 +0100

    xml loader pierwszy

--- DisplayOferty/myscript.js
+++ DisplayOferty/myscript.js
@@ -2,22 +2,42 @@ $(document).ready(function(){
   function Oferta(name, zawod) {
     var self = this;
     self.name = name;
-    self.zawod = ko.observable(zawod);
+    self.zawod = zawod;
 }
   function viewModel() {
 
      this.dayOfWeek = ko.observable('Sunday');
      this.oferts=ko.observableArray([
-     	new Oferta("Sprzataczka", "spra"),
-     	new Oferta("Kelner", "draaa")
      	]);
+
+       let ogloszenia=[];
+       $.ajax({
+        type:"GET",
+         url:'data.xml',
+         async: true,
+        dataType: 'xml',
+        success: function(response){
+          let MainXml=$(response).find('payload').text();
+          let xmlDoc=$.parseXML(MainXml);
+          let MainOferts =$(xmlDoc).find('PositionOpening').find('PositionProfile').each(function(){
+            let id=$(this).find('PositionNumberPL').text();
+            let zawod=$(this).find('ProfileName').text();
+             let oferta = new Oferta(id, zawod);
+             ogloszenia.push(oferta);
+             console.log(oferta);
+          });
+          console.log("xml loaded successfull");
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown){
+          console.log("error! Loading xml faild");
+        }
+     });
+      console.log("oferta: ")
   		
   };
 
+
   ko.applyBindings(new viewModel()); 
 
-   $("h2").click(function(){
-   		console.log("click");
-   });
 
 }); 
